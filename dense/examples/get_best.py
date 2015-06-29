1304739bc56b40dd952a302ccd0ce2584174346a
AmirooR <noname01.cpp@gmail.com>
2015-06-29 15:34:48 +0200

    bug fixed in get_best.py

--- dense/examples/get_best.py
+++ dense/examples/get_best.py
@@ -6,7 +6,10 @@ in_dir = sys.argv[1]
 a = os.listdir(in_dir)
 best_pr = -1
 total = -1
+best_name = ''
 for x in a:
+#    if x.startswith('best'):
+#        os.remove(in_dir+x)
     if x.endswith('_output.txt'):
         print x
         f = open(in_dir+x)
@@ -15,7 +18,8 @@ for x in a:
         total = float(b[1])
         if best_pr < pr:
             best_pr = pr
+            best_name = x
         f.close()
-ff = open(in_dir+'best.txt','w')
+ff = open(in_dir+'best_'+best_name[:-4]+'.txt','w')
 ff.write(str(best_pr)+'/'+str(total)+'\n')
 ff.close()
